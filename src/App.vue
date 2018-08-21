d7be0047682559173f861ede6917e3ff3680b83d
niejian <>
2018-08-21 13:27:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+     <Header/>
+     <Menu/>
+     <Main/>
+  </div>
+</template>
+
+<script>
+//Menu-- 左侧菜单栏
+import Menu from './components/main/Menu.vue'
+import Header from './components/main/Header.vue'
+import Main from './components/main/Main.vue'
+
+
+
+
+export default {
+  name: 'App',
+  //注册主键
+  components: {
+        Menu,
+        Header,
+        Main
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/main/Main.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main-class">
+        <el-main>
+            <el-table>
+                <el-table-column prop="date" label="日期" width="140">
+                </el-table-column>
+                <el-table-column prop="name" label="姓名" width="120">
+                </el-table-column>
+                <el-table-column prop="address" label="地址">
+                </el-table-column>
+            </el-table>
+        </el-main>
+    </div>
+    
+</template>
+
+<style>
+.mai2n-class{
+    float: right;
+    widows: 1000%;
+    margin: 0;
+}
+</style>
+
